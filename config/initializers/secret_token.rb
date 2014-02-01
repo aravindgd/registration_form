4fb67e4e700cde78b3660c01b701d8afeb74d99f
aravind <pro.aravind@gmail.com>
2014-02-01 14:57:11 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SboaRegisterationProject::Application.config.secret_key_base = 'e3af408cdd50bcf4e03b05411f04f43e0a3641803accf901b457acfbdd2c403b7ae7c0206dac25c3f091cd765eec27f9bb52589f9552ca58edaa6cb7f4c43899'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SboaRegisterationProject::Application.config.session_store :cookie_store, key: '_sboa_registeration_project_session'
