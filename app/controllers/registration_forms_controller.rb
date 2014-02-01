f679114f969db99513d6e2646c996794de1aa18d
aravind <pro.aravind@gmail.com>
2014-02-01 19:55:31 +0530

    upload and basic functionality is working

--- app/controllers/registration_forms_controller.rb
+++ app/controllers/registration_forms_controller.rb
@@ -24,8 +24,10 @@ class RegistrationFormsController < ApplicationController
   # POST /registration_forms
   # POST /registration_forms.json
   def create
-    @registration_form = RegistrationForm.new(registration_form_params)
-
+    @registration_form = RegistrationForm.new(name_of_student: params[:registration_form][:name_of_student],gender: params[:registration_form][:gender],nationality:  params[:registration_form][:nationality],caste: params[:registration_form][:caste],community_category: params[:registration_form][:community_category],child_with_special_needs: params[:registration_form][:child_with_special_needs],locality: params[:registration_form][:locality],residental_address: params[:registration_form][:residental_address],pincode: params[:registration_form][:pincode],landmark: params[:registration_form][:landmark],mobile_number: params[:registration_form][:mobile_number],landline_number: params[:registration_form][:landline_number],email: params[:registration_form][:email])
+    @registration_form.date_of_birth_proof = params[:registration_form][:date_of_birth_proof]
+    @registration_form.build_father_data(name: params[:registration_form][:father_datas][:name],qualification: params[:registration_form][:father_datas][:qualification],qualification_proof: params[:registration_form][:father_datas][:qualification_proof],annual_income: params[:registration_form][:father_datas][:annual_income],occupation_designation: params[:registration_form][:father_datas][:occupation_designation])
+    @registration_form.build_mother_data(name: params[:registration_form][:mother_datas][:name],qualification: params[:registration_form][:mother_datas][:qualification],qualification_proof: params[:registration_form][:mother_datas][:qualification_proof],annual_income: params[:registration_form][:mother_datas][:annual_income],occupation_designation: params[:registration_form][:mother_datas][:occupation_designation])
     respond_to do |format|
       if @registration_form.save
         format.html { redirect_to @registration_form, notice: 'Registration form was successfully created.' }
