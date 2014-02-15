27b9437d9c91ba4f43bf51eca488fd8b063f9623
Karthick <karthick@gmail.com>
2014-02-15 19:01:31 +0530

    migration file removed

--- db/migrate/20140211204145_changesiblingtype.rb
+++
@@ -1,6 +0,0 @@
-class Changesiblingtype < ActiveRecord::Migration
-  def change
-  	change_column :registration_forms, :sibling, :string
-  	change_column :registration_forms, :child_special, :string
-  end
-end
